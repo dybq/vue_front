584f67670c8f3d614a4e870ac0a544b5d8743b19
dybq <danyubaoqi@gmail.com>
2021-02-16 14:04:16 +0900

    111

---
+++ src/components/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+    <el-table-column
+        prop="username"
+        label="团长"
+        width="180">
+    </el-table-column>
+    <el-table-column
+        prop="datetime"
+        label="时间"
+        width="180">
+    </el-table-column>
+    <el-table-column
+        prop="diff"
+        label="难度"
+        width="180">
+    </el-table-column>
+    <el-table-column
+        prop="remark"
+        label="备注"
+        width="1000">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+
+export default {
+  name: 'index',
+  data() {
+    return {
+      tableData: []
+
+    }
+  },
+  created() {
+    this.getlist()
+  },
+  methods: {
+    getlist() {
+      let _this = this
+      axios.get('http://localhost:5000/api/getdata').then(response => {
+        _this.tableData = response.data
+      })
+    }
+  }
+
+}</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import axios from "axios";
+import VueAxios from 'vue-axios'
+axios.defaults.baseURL = ''
+// axios.defaults.headers.get["Content-type"]="application/json";
+axios.interceptors.request.use(
+    config => {
 
+        config.headers.common['Access-Control-Allow-Origin'] ='*' ;
+        config.headers.common['Access-Control-Allow-Method'] ='GET,POST' ;
+        return config}
+
+)
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
